ca0bd0825a23de655a04bb6e20ed988e4e36d5b9
flashls1 <imflash144@gmail.com>
2026-03-11 10:57:40 -0500

    fix: desktop sparkle 3px left + 3px up

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -127,8 +127,8 @@ const AnimatedLogo = ({ duration = 2.5, className = '' }: AnimatedLogoProps) => 
                     opacity: 0,
                 },
                 {
-                    top: isMobile ? '-7%' : '-2%',
-                    left: isMobile ? '58%' : '59%',
+                    top: isMobile ? '-7%' : '-3%',
+                    left: isMobile ? '58%' : '58%',
                     scale: isMobile ? 0.4 : 0.5,
                     opacity: 0.6,
                     duration: duration * 0.35,
